1f92756aa46d152b5a5d2078beefe3eb05d8d31c
DanielUtec123 <daniel.rojas@utec.edu.pe>
2020-06-11 20:55:20 -0500

    register end point working, getters and setters of User complete

--- Application/web/openbook/src/main/java/com/software/controller/AuthController.java
+++ Application/web/openbook/src/main/java/com/software/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.software.controller;
 
+import com.software.model.Professor;
+import com.software.model.Student;
 import com.software.model.User;
 import com.software.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -61,20 +63,34 @@ public class AuthController {
 
 
     @RequestMapping("/register")
-    public String register(){
-        // TO DO
-        //Return the register page
-        return "Registration page";
+    public String register(Model model){
+        return "register";
     }
 
 
-    @PostMapping(value = "/do_register")
-    public String do_register(@ModelAttribute User user){
+    @PostMapping(value = "/do_register_student")
+    public String do_register_student(@ModelAttribute Student student){
+        // TO DO
+
+        if(authService.registerUser(student))
+            return "index";
+        return "error";
+    }
+
+    @PostMapping(value = "/do_register_profesor")
+    public String do_register_profesor(@ModelAttribute Professor professor){
+        // TO DO
+        if(authService.registerUser(professor))
+            return "User Register";
+        return "This email is used by other user";
+    }
+
+    @PostMapping(value = "/do_register_curador")
+    public String do_register_curador(@ModelAttribute User user){
         // TO DO
         if(authService.registerUser(user))
             return "User Register";
         return "This email is used by other user";
-
     }
 
 }
--- Application/web/openbook/src/main/java/com/software/model/User.java
+++ Application/web/openbook/src/main/java/com/software/model/User.java
@@ -67,4 +67,28 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
